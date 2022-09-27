9daeb9773b7015e6470c6747e49852a53ff02525
harshjainsk <harshjainsk531@gmail.com>
2022-09-27 11:13:28 +0530

    added bounding boxes and shows area

--- main.py
+++ main.py
@@ -52,42 +52,55 @@ def getContours(img, imgContour):
     for cnt in contours:
         area = cv2.contourArea(cnt)
 
-        if area > 10:
+        areaMin = cv2.getTrackbarPos("Area", "PARAMETERS")
+        if area > areaMin:
             cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
             peri = cv2.arcLength(cnt, True)  # True says that contour is closed
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            print(len(approx))
+
+            # Drawing Bounding Box
+            x, y, width, height = cv2.boundingRect(approx)
+            cv2.rectangle(imgContour, (x, y), (x + width, y + height), (0, 255, 0), 5)
+
+            cv2.putText(imgContour, "Points: " + str(len(approx)), (x + width + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX,
+                        0.7, (0, 255, 0), 2)
+            cv2.putText(imgContour, "Area: " + str(int(area)), (x + width + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX,
+                        0.7, (0, 255, 0), 2)
 
 
 cv2.namedWindow("PARAMETERS")
 cv2.resizeWindow("PARAMETERS", 640, 240)
 cv2.createTrackbar("Threshold1", "PARAMETERS", 0, 255, empty)
 cv2.createTrackbar("Threshold2", "PARAMETERS", 255, 255, empty)
+cv2.createTrackbar('Area', "PARAMETERS", 10, 200, empty)
 
 frameWidth = 640
 frameHeight = 400
-# cap = cv2.VideoCapture(0)
-# cap.set(3, frameWidth)
-# cap.set(4, frameHeight)
-path = 'resources/rectangle.png'
-img = cv2.imread(path)
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+# path = 'resources/shapes.jpg'
+# img = cv2.imread(path)
+# img = cv2.resize(img, (600, 400))
 
 # 137 0
 # 16 109
 
 while True:
 
-    # ret, img = cap.read()
+    ret, img = cap.read()
 
     imgContour = img.copy()
 
     imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
     imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
 
+    # getting values from trackBar
     threshold1 = cv2.getTrackbarPos("Threshold1", "PARAMETERS")
     threshold2 = cv2.getTrackbarPos("Threshold2", "PARAMETERS")
-    print(threshold1, threshold2)
-    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
 
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
     kernel = np.ones((5, 5))
     imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
 
